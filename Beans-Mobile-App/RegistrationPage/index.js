043cdb245881f3196c48753efc33bb8be64dc106
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-04 14:48:09 +0100

    ...

---
+++ Beans-Mobile-App/RegistrationPage/index.js
@@ -0,0 +1,5 @@
+// console.log(document.getElementsByTagName("Body")[0].innerHTML);
+document.addEventListener("DOMContentLoaded", () =>{
+    let button = document.getElementsByClassName("register_btn")
+    console.log(button)
+})
