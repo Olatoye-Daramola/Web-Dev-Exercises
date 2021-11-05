3cfea4ad45b35fdad8f31fd7b3a0ae18f9937cee
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-05 11:19:41 +0100

    ...

--- Beans-Mobile-App/RegistrationPage/index.js
+++ Beans-Mobile-App/RegistrationPage/index.js
@@ -1,5 +1,35 @@
-// console.log(document.getElementsByTagName("Body")[0].innerHTML);
+// // console.log(document.getElementsByTagName("Body")[0].innerHTML);
 document.addEventListener("DOMContentLoaded", () =>{
-    let button = document.getElementsByClassName("register_btn")
-    console.log(button)
+    // ButtonElement
+    let button = document.getElementsByClassName("register_btn")[0]
+    button.addEventListener("click", () => console.log("register_btn clicked"));
+
+    // InputFields
+    function handleInputChange(e) {
+        console.log(e.target.name, e.target.value)
+    }
+    let inputFields = document.getElementsByTagName("input")
+    // for (let i = 0; i < inputFields.length; i++) {
+    //     inputFields[i].addEventListener("input", (e) => handleInputChange(e))
+    // }
+    Array.from(inputFields).forEach((inputField) => inputField.addEventListener("input", (e) => handleInputChange(e)))
+
 })
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let registerObject = {}
+//
+//     // ButtonElement
+//     let button = document.getElementsByClassName("register_btn")[0]
+//     button.addEventListener("click", () => console.log(registerObject))
+//
+//     // InputFields
+//     function handleInputChange(e) {
+//         registerObject = {...registerObject, [e.target.name]: e.target.value}
+//     }
+//
+//     let inputFields = document.getElementsByTagName("input")
+//     Array.from(inputFields).forEach((inputField) =>
+//         inputField.addEventListener("input", (e) => handleInputChange(e)))
+// })
