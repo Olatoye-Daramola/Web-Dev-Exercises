603adb5d59978db7fd21603bf18fb5941fc2b13a
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-10 15:43:56 +0100

    ...

---
+++ Beans-Mobile-App/HomePage/home.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    let headingDiv       = document.getElementsByClassName("user-holla");
+    headingDiv.innerHTML = "<H3>Public Offers</H3>";
+})
